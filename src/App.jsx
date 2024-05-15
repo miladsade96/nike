1122588bda6960275ba9e0fbe47a8898f7aae97f
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-15 11:31:42 +0330

    Structuring app.jsx component sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-stone-500">
-      Hello world!
-    </h1>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special Offer</section>
+      <section className="padding bg-pale-blue">Customer Reviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8 text-white">
+        Footer
+      </section>
+    </main>
   );
 }
