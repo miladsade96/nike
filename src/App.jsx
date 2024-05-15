4e57fe6798caecf74ea8f2e1935085efe0a1405d
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-15 11:47:35 +0330

    Adding section components to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
+import {
+  Hero,
+  PopularProducts,
+  Footer,
+  Subscribe,
+  Services,
+  SuperQuality,
+  SpecialOffer,
+  CustomerReviews,
+} from "./sections/index.js";
+
 export default function App() {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">Popular Products</section>
-      <section className="padding">Super Quality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">Special Offer</section>
-      <section className="padding bg-pale-blue">Customer Reviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      {/*Nav*/}
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
       <section className="bg-black padding-x padding-t pb-8 text-white">
-        Footer
+        <Footer />
       </section>
     </main>
   );
