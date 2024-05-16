5adf3b3da527e040a3b0bb847c896d96476757cc
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-16 16:52:54 +0330

    Implementing navbar component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,32 @@
+import { headerLogo } from "../assets/images/index.js";
+import { navLinks } from "../constants/index.js";
+import { hamburger } from "../assets/icons/index.js";
+
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Nike Logo" width={130} height={29} />
+        </a>
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => {
+            return (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-montserrat leading-normal text-lg text-slate-gray"
+                >
+                  {item.label}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hamburger Menu" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
 }
