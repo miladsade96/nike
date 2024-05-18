e7d318a2a51f1ac2a1840874911aa6e3726ecd39
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-18 11:32:22 +0330

    Implementing ShoeCard component

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,3 +1,26 @@
-export default function ShoeCard() {
-  return <div>ShoeCard Component</div>;
+export default function ShoeCard({ imgUrl, bigShoeImage, changeBigShoeImage }) {
+  function handleClick() {
+    if (bigShoeImage !== imgUrl.bigShoe) changeBigShoeImage(imgUrl.bigShoe);
+  }
+
+  return (
+    <div
+      className={`border-2 rounded-xl ${
+        bigShoeImage === imgUrl.bigShoe
+          ? "border-coral-red"
+          : "border-transparent"
+      } cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
+    >
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:size-40 rounded-xl max-sm:p-4">
+        <img
+          src={imgUrl.thumbnail}
+          alt="Shoe Collection"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
 }
