ce0f5dbc84ecd13149f74f2e895fdc1c99b3381c
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-24 14:05:37 +0330

    Using serviceCard component to implement Services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,3 +1,12 @@
+import { services } from "../constants/index.js";
+import ServiceCard from "../components/ServiceCard.jsx";
+
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.imgURL} {...service} />
+      ))}
+    </section>
+  );
 }
